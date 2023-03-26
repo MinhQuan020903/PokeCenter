363417501fc9ba2420bcaeae286b89477a01ec17
petrichor-hl <tl.hoanglam174@gmail.com>
2023-03-26 21:56:07 +0700

    move Fragment

--- app/src/main/java/com/example/pokecenter/customer/lam/CustomerFragment.java
+++ app/src/main/java/com/example/pokecenter/customer/lam/CustomerFragment.java
@@ -1,4 +1,4 @@
-package com.example.pokecenter;
+package com.example.pokecenter.customer.lam;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.pokecenter.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AdminFragment#newInstance} factory method to
+ * Use the {@link CustomerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AdminFragment extends Fragment {
+public class CustomerFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class AdminFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public AdminFragment() {
+    public CustomerFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class AdminFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminFragment.
+     * @return A new instance of fragment CustomerFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AdminFragment newInstance(String param1, String param2) {
-        AdminFragment fragment = new AdminFragment();
+    public static CustomerFragment newInstance(String param1, String param2) {
+        CustomerFragment fragment = new CustomerFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class AdminFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin, container, false);
+        return inflater.inflate(R.layout.fragment_customer, container, false);
     }
 }
--- app/src/main/java/com/example/pokecenter/vender/VenderFragment.java
+++ app/src/main/java/com/example/pokecenter/vender/VenderFragment.java
@@ -1,4 +1,4 @@
-package com.example.pokecenter;
+package com.example.pokecenter.vender;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.pokecenter.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CustomerFragment#newInstance} factory method to
+ * Use the {@link VenderFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CustomerFragment extends Fragment {
+public class VenderFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class CustomerFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CustomerFragment() {
+    public VenderFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class CustomerFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomerFragment.
+     * @return A new instance of fragment VenderFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CustomerFragment newInstance(String param1, String param2) {
-        CustomerFragment fragment = new CustomerFragment();
+    public static VenderFragment newInstance(String param1, String param2) {
+        VenderFragment fragment = new VenderFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class CustomerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer, container, false);
+        return inflater.inflate(R.layout.fragment_vender, container, false);
     }
 }
