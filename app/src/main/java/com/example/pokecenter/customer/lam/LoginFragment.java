46f5d6da40e7b9e9c0705cad8708b8039a81e57d
petrichor-hl <tl.hoanglam174@gmail.com>
2023-03-27 00:07:07 +0700

    add BottomNav Customer

--- app/src/main/java/com/example/pokecenter/customer/lam/LoginFragment.java
+++ app/src/main/java/com/example/pokecenter/customer/lam/LoginFragment.java
@@ -5,6 +5,8 @@ import static androidx.core.content.ContextCompat.getSystemService;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -28,11 +30,6 @@ import com.example.pokecenter.databinding.FragmentLoginBinding;
  */
 public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
     private FragmentLoginBinding binding;
 
     private Account[] mockAccounts = {
@@ -40,6 +37,11 @@ public class LoginFragment extends Fragment {
             new Account("vender1", "123", 1),
             new Account("admin1", "123", 2)
     };
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -167,6 +169,12 @@ public class LoginFragment extends Fragment {
             }
         }
         return -1;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 
     @Override
