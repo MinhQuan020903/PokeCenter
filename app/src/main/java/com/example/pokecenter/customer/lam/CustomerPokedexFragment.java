ccf98142341b1a26d21782bcf714589587108af8
petrichor-hl <tl.hoanglam174@gmail.com>
2023-03-30 08:31:28 +0700

    backup point

--- app/src/main/java/com/example/pokecenter/customer/lam/CustomerPokedexFragment.java
+++ app/src/main/java/com/example/pokecenter/customer/lam/CustomerPokedexFragment.java
@@ -1,4 +1,4 @@
-package com.example.pokecenter;
+package com.example.pokecenter.customer.lam;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.pokecenter.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CustomerShoppingCartFragment#newInstance} factory method to
+ * Use the {@link CustomerPokedexFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CustomerShoppingCartFragment extends Fragment {
+public class CustomerPokedexFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class CustomerShoppingCartFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CustomerShoppingCartFragment() {
+    public CustomerPokedexFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class CustomerShoppingCartFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomerShoppingCartFragment.
+     * @return A new instance of fragment CustomerPokedexFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CustomerShoppingCartFragment newInstance(String param1, String param2) {
-        CustomerShoppingCartFragment fragment = new CustomerShoppingCartFragment();
+    public static CustomerPokedexFragment newInstance(String param1, String param2) {
+        CustomerPokedexFragment fragment = new CustomerPokedexFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class CustomerShoppingCartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_shopping_cart, container, false);
+        return inflater.inflate(R.layout.fragment_customer_pokedex, container, false);
     }
 }
