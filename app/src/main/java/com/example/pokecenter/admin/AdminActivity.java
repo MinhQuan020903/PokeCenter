61285999d641d3eb3cc34de460afaccf1bc8062f
Đỗ Mai Minh Quân <21522488@gm.uit.edu.vn>
2023-05-12 23:06:08 +0700

    Quan - Add admin fragments

--- app/src/main/java/com/example/pokecenter/admin/AdminActivity.java
+++ app/src/main/java/com/example/pokecenter/admin/AdminActivity.java
@@ -1,31 +1,112 @@
 package com.example.pokecenter.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.pokecenter.admin.Quan.AdminTab.Home.AdminHomeFragment;
+import com.example.pokecenter.admin.Quan.AdminTab.Notification.AdminNotificationFragment;
+import com.example.pokecenter.admin.Quan.AdminTab.Profile.AdminProfileFragment;
+import com.example.pokecenter.admin.Quan.AdminTab.Support.AdminSupportFragment;
+import com.example.pokecenter.customer.lam.CustomerTab.Cart.CustomerShoppingCartFragment;
+import com.example.pokecenter.customer.lam.CustomerTab.CustomerOrdersFragment;
+import com.example.pokecenter.customer.lam.CustomerTab.Home.CustomerHomeFragment;
+import com.example.pokecenter.customer.lam.CustomerTab.Notification.CustomerNotificationsFragment;
+import com.example.pokecenter.customer.lam.CustomerTab.Profile.CustomerProfileFragment;
+import com.example.pokecenter.databinding.ActivityAdminBinding;
+import com.example.pokecenter.databinding.ActivityCustomerBinding;
 import com.example.pokecenter.vender.Ninh.VenderTab.Home.VenderHomeFragment;
 import com.example.pokecenter.vender.Ninh.VenderTab.Home.VenderStatisticsFragment;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 import com.example.pokecenter.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AdminActivity extends AppCompatActivity {
 
+    private ActivityAdminBinding binding;
+    public static BottomNavigationView adminBottomNavigationView;
+
     @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
 
-//        // Thay vì setContentView, sử dụng FragmentManager để hiển thị fragment đầu tiên
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.replace(android.R.id.content, new VenderStatisticsFragment());
-//        fragmentTransaction.commit();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Window window = getWindow();
+            window.setStatusBarColor(getColor(R.color.light_canvas));
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        binding = ActivityAdminBinding.inflate(getLayoutInflater());
+        adminBottomNavigationView = binding.adminBottomNavView;
+
+        Fragment home = new AdminHomeFragment();
+        Fragment support = new AdminSupportFragment();
+        Fragment notification = new AdminNotificationFragment();
+        Fragment profile = new AdminProfileFragment();
+
+        // Move between fragments
+        adminBottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.adminHomeFragment:
+                    replaceFragment(home);
+                    break;
+                case R.id.adminSupportFragment:
+                    replaceFragment(support);
+                    break;
+                case R.id.adminNotificationFragment:
+                    replaceFragment(notification);
+                    break;
+                case R.id.adminProfileFragment:
+                    replaceFragment(profile);
+                    break;
+            }
+            return true;
+        });
+
+        /*
+        Lấy giá trị targetedFragment để check xem ở một nơi nào đó trong app
+        mà move tới CustomerActivity với Fragment mong muốn nào
+
+        Set HomeFragment is default
+
+        Đồng thời lúc này customerBottomNavigationView.setOnItemSelectedListener() ở dòng 51
+        cũng sẽ được trigger
+        => Thực thi lệnh "replaceFragment(new CustomerHomePlaceholderFragment());"
+        => Nội dung page cũng sẽ thay đổi theo
+         */
+
+        adminBottomNavigationView.setSelectedItemId(getIntent().getIntExtra("targetedFragment", R.id.adminHomeFragment));
+        setContentView(binding.getRoot());
+    }
+
+    private void replaceFragment(Fragment selectedFragment) {
+
+        /*
+        Notes:
+            getFragmentManager() was deprecated in API level 28.
+
+            we will use getChildFragmentManager() Return a private FragmentManager for placing and managing Fragments inside of this Fragment.
+
+            we will use getParentFragmentManager() Return the FragmentManager for interacting with fragments associated with this fragment's activity.
+
+            so, if you deal with fragments inside a fragment you will use the first one and if you deal with fragments inside an activity you will use the second one.
+
+            you can find them here package androidx.fragment.app;
+         */
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentAdmin, selectedFragment);
+        fragmentTransaction.commit();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/example/pokecenter/admin/Quan/AdminTab/Home/AdminHomeFragment.java
@@ -0,0 +1,33 @@
+package com.example.pokecenter.admin.Quan.AdminTab.Home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pokecenter.R;
+import com.example.pokecenter.databinding.FragmentAdminHomeBinding;
+
+public class AdminHomeFragment extends Fragment {
+
+    private FragmentAdminHomeBinding binding;
+    public AdminHomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentAdminHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
