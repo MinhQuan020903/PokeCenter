1bdcfe1ca2f3659abb4a0713204886570e98be5f
Ninhnon <nguyentrongninh24072003@gmail.com>
2023-05-28 16:17:12 +0700

    devNinh: add vender product list

---
+++ app/src/main/java/com/example/pokecenter/vender/VenderTab/Home/Product/VenderProductDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.pokecenter.vender.VenderTab.Home.Product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VenderProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vender_product_detail);
+    }
+}
